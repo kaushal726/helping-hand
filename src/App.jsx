31eea93466dcebc4019f65bb657655589fca90a3
Kaushal <Kausraj11@gmail.com>
2023-08-23 08:25:39 +0530

    ADDED TO REPO

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center w-full h-screen">
+        <h1 className="text-5xl font-bold underline">Hello world!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
